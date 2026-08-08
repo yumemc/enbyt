c7646be83149030da499e1678c6a408efa6153eb
alex <alex.podikoglou@gmail.com>
2026-08-09 00:13:57 +0300

    feat: Tag and TagPayload types

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,33 @@
+use std::collections::HashMap;
 
+/// enbyt: a Rust NBT library
+///
+/// NBT Format Reference: https://minecraft.wiki/w/NBT_format
+
+#[derive(Debug)]
+pub struct Tag {
+    name: String,
+    payload: TagPayload,
+}
+
+#[derive(Debug)]
+pub enum TagPayload {
+    Byte(i8),
+    Boolean(bool),
+    Short(i16),
+    Int(i32),
+    Long(i64),
+    Float(f32),
+    Double(f64),
+    String(String),
+
+    // NOTE: Should be homogenous
+    List(Vec<Tag>),
+
+    // NOTE: Must hold that key == value.name
+    Compound(HashMap<String, Tag>),
+
+    ByteArray(Vec<u8>),
+    IntArray(Vec<i32>),
+    LongArray(Vec<i64>),
+}
