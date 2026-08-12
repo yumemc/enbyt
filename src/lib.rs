b03a8a99e16c17508728aa8552c60c66548de24a
alex <alex.podikoglou@gmail.com>
2026-08-13 00:21:25 +0300

    test: test_write_string_value

--- src/lib.rs
+++ src/lib.rs
@@ -98,6 +98,19 @@ pub mod binary {
 
                 assert_eq!(len, str.len());
             }
+
+            #[hegel::test]
+            fn test_write_string_value(tc: TestCase) {
+                let str = tc.draw(gs::text());
+
+                let mut buf = vec![0; 2 + str.len()];
+                write_string(&mut buf, str.clone());
+
+                let str_bytes = &buf[2..];
+                let str_decoded = String::from_utf8(str_bytes.into());
+
+                assert_eq!(str_decoded, Ok(str));
+            }
         }
     }
 
