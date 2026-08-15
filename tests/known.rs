00e8b3fe0ae9122d5cbb587d6f2ed6bfb6e36112
alex <alex.podikoglou@gmail.com>
2026-08-15 03:25:52 +0300

    test: test heterogeneous and homogeneous lists

--- tests/known.rs
+++ tests/known.rs
@@ -1,4 +1,7 @@
-use enbyt::{Tag, TagPayload, binary::deserialize::parse_tag};
+use enbyt::{
+    Tag, TagPayload,
+    binary::{deserialize::parse_tag, serialize::write_tag},
+};
 
 #[test]
 fn test_empty_tag() {
@@ -8,4 +11,48 @@ fn test_empty_tag() {
         parse_tag(&mut input),
         Ok(Tag::new(None, TagPayload::Empty).unwrap())
     );
+}
+
+#[test]
+fn test_heterogenous_list() {
+    let mut buf = vec![0; 12];
+
+    let list = Tag::new(
+        Some("list".to_string()),
+        TagPayload::List(
+            0x03,
+            vec![
+                Tag::new(Some("a".to_string()), TagPayload::String("aa".to_string())).unwrap(),
+                Tag::new(Some("b".to_string()), TagPayload::Byte(4)).unwrap(),
+            ],
+        ),
+    )
+    .unwrap();
+
+    assert_eq!(
+        write_tag(&mut buf, list),
+        Err(enbyt::NBTError::UnexpectedType(
+            0x03,
+            Tag::new(Some("a".to_string()), TagPayload::String("aa".to_string())).unwrap(),
+        ))
+    );
+}
+
+#[test]
+fn test_homogenous_list() {
+    let mut buf = vec![0; 12];
+
+    let list = Tag::new(
+        Some("list".to_string()),
+        TagPayload::List(
+            0x03,
+            vec![
+                Tag::new(Some("a".to_string()), TagPayload::Int(3)).unwrap(),
+                Tag::new(Some("b".to_string()), TagPayload::Int(4)).unwrap(),
+            ],
+        ),
+    )
+    .unwrap();
+
+    assert!(write_tag(&mut buf, list).is_ok());
 }
