36dcade20d1e1978a2084fe2d51a2494a4f51aa5
alex <alex.podikoglou@gmail.com>
2026-08-17 23:21:12 +0300

    bench: rename bench to deserialization; add raw benchmark

---
+++ benches/deserialization.rs
@@ -0,0 +1,24 @@
+use std::hint::black_box;
+
+use criterion::{Criterion, criterion_group, criterion_main};
+use enbyt::binary::deserialize;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let bytes_compressed = include_bytes!("../tests/samples/level.dat");
+    let bytes_raw = include_bytes!("../tests/samples/level.dat.raw");
+
+    c.bench_function("deserialize compressed", |b| {
+        b.iter(|| {
+            let _ = black_box(deserialize::parse_compressed_tag(&bytes_compressed[..]));
+        })
+    });
+
+    c.bench_function("deserialize raw", |b| {
+        b.iter(|| {
+            let _ = black_box(deserialize::parse_tag(&mut &bytes_raw[..]));
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
