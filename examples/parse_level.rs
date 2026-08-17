88091ab7d31085eabc4d840a4e13912d94cfa3a3
alex <alex.podikoglou@gmail.com>
2026-08-17 12:58:10 +0300

    refactor: clean up example

--- examples/parse_level.rs
+++ examples/parse_level.rs
@@ -1,10 +1,8 @@
-use std::fs::File;
-
 use enbyt::binary::deserialize::parse_compressed_tag;
 
 fn main() {
-    let file = include_bytes!("level.dat");
-    let tag = parse_compressed_tag(&file[..]).expect("couldn't parse tag");
+    let bytes = include_bytes!("level.dat");
+    let tag = parse_compressed_tag(&bytes[..]).expect("couldn't parse tag");
 
     dbg!(tag);
 }
