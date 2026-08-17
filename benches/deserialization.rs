475a72becda31da4b947a7ab8c31843e2f2911f7
alex <alex.podikoglou@gmail.com>
2026-08-18 00:02:08 +0300

    bench: include_bytes! in place

--- benches/deserialization.rs
+++ benches/deserialization.rs
@@ -4,18 +4,19 @@ use criterion::{Criterion, criterion_group, criterion_main};
 use enbyt::binary::deserialize;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let bytes_compressed = include_bytes!("../tests/samples/level.dat");
-    let bytes_raw = include_bytes!("../tests/samples/level.dat.raw");
-
     c.bench_function("deserialize compressed", |b| {
         b.iter(|| {
-            black_box(deserialize::parse_compressed_tag(&bytes_compressed[..])).unwrap();
+            let bytes = include_bytes!("../tests/samples/level.dat");
+
+            black_box(deserialize::parse_compressed_tag(&bytes[..])).unwrap();
         })
     });
 
     c.bench_function("deserialize raw", |b| {
         b.iter(|| {
-            black_box(deserialize::parse_tag(&mut &bytes_raw[..])).unwrap();
+            let bytes = include_bytes!("../tests/samples/level.dat.raw");
+
+            black_box(deserialize::parse_tag(&mut &bytes[..])).unwrap();
         })
     });
 }
