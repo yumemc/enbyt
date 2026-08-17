2e81d858796584b09b50e73bb8f8c72816e19b85
alex <alex.podikoglou@gmail.com>
2026-08-17 03:04:21 +0300

    feat: implement PartialeEq for NBTError

--- src/error.rs
+++ src/error.rs
@@ -24,3 +24,22 @@ pub enum NBTError {
     #[error("io error: {0:?}")]
     IO(#[from] io::Error),
 }
+
+impl PartialEq for NBTError {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (NBTError::InvalidTagName(a), NBTError::InvalidTagName(b)) => a == b,
+            (NBTError::UnexpectedType(a_type, a_tag), NBTError::UnexpectedType(b_type, b_tag)) => {
+                a_type == b_type && a_tag == b_tag
+            }
+            (NBTError::InconsistentCompound, NBTError::InconsistentCompound) => true,
+            (NBTError::InconsistentList, NBTError::InconsistentList) => true,
+            (NBTError::InvalidPayloadType, NBTError::InvalidPayloadType) => true,
+
+            // not sure if it's correct but probably best we can do
+            (NBTError::IO(a), NBTError::IO(b)) => a.kind() == b.kind(),
+
+            _ => false,
+        }
+    }
+}
