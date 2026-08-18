946aee022a079bc2a51f180678e94a391df9e5db
alex <alex.podikoglou@gmail.com>
2026-08-18 12:37:59 +0300

    test: update generator to generate proper compounds

--- tests/shared/generators.rs
+++ tests/shared/generators.rs
@@ -26,7 +26,7 @@ pub fn generate_tag_payload(tc: &TestCase) -> TagPayload {
             ),
             tc.draw(gs::vecs(generate_tag_payload())),
         ),
-        0x0a => TagPayload::Compound(tc.draw(gs::hashmaps(gs::text(), generate_tag()))),
+        0x0a => TagPayload::Compound(tc.draw(gs::hashmaps(gs::text(), generate_tag_payload()))),
         0x0b => TagPayload::IntArray(tc.draw(gs::vecs(gs::integers::<i32>()))),
         0x0c => TagPayload::LongArray(tc.draw(gs::vecs(gs::integers::<i64>()))),
         _ => unreachable!(),
