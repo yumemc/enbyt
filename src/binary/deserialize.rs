0a5b25a18bdd453300aad02681f3b36e026cae4d
alex <alex.podikoglou@gmail.com>
2026-08-20 14:02:58 +0300

    refactor: clean up parse_end_payload

--- src/binary/deserialize.rs
+++ src/binary/deserialize.rs
@@ -134,11 +134,9 @@ pub fn parse_list_payload(input: &mut &[u8]) -> ModalResult<(TagPayloadType, Vec
 
 /// Parses an NBT end tag's payload.
 pub fn parse_end_payload(input: &mut &[u8]) -> ModalResult<()> {
-    any.context(StrContext::Label("end marker"))
-        .verify(|&x| x == 0x00)
-        .parse_next(input)?;
-
-    Ok(())
+    0x00.context(StrContext::Label("end marker"))
+        .parse_next(input)
+        .map(|_| ())
 }
 
 /// Parses a NBT compound tag's payload into a [`BTreeMap<String, TagPayload>`], mapping the names
